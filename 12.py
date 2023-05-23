aeb890c60dec651566fe204505eeb41af86be0c6
Asy_student <asb_mogilev@mail.ru>
2023-05-23 16:50:22 +0300

    добавления идеального решения в 12 задачу

--- 12.py
+++ 12.py
@@ -11,11 +11,19 @@
 # 4 4 -> 2 2
 # 5 6 -> 2 3
 
-num_sum = int(input('Введите первую подсказку, сумму загаданных чисел: '))
-num_composition = int(input('Введите вторую подсказку, произведение загаданных чисел: '))
-discriminant = num_sum ** 2 - 4 * num_composition
+# num_sum = int(input('Введите первую подсказку, сумму загаданных чисел: '))
+# num_composition = int(input('Введите вторую подсказку, произведение загаданных чисел: '))
+# discriminant = num_sum ** 2 - 4 * num_composition
 
-y = num_sum // 2
-x = num_sum - y
-print(f'Число Х = {x}, число  Y = {y}')
+# y = num_sum // 2
+# x = num_sum - y
+# print(f'Число Х = {x}, число  Y = {y}')
+
+# Пример идеального решения
+x = int(input())
+y = int(input())
+for i in range(x):
+    for j in range(y):
+        if x == i + j and y == i * j:
+            print(i, j)
 
