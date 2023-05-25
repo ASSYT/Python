5a943fa84b96af7db90eda5652d3944ca3d44af8
Asy_student <asb_mogilev@mail.ru>
2023-05-25 21:56:59 +0300

    задача 17 с добавление решения на уроке

--- 17.py
+++ 17.py
@@ -10,14 +10,28 @@
 
 list_1 = [1, 1, 2, 0, -1, 3, 4, 4]
 # list_1 = [1, 2, 4, 0, 1, -1, 3, 4, 4, 1, 5]
-print(list_1)
-i = 0
-j = len(list_1)-1
-n = 0
-while i <= len(list_1)-1:
-    while j >= i:
-        if list_1[i] != list_1[j]:
-            n += 1
-        j -= 1 
-    i += 1           
-print(n)
+# print(list_1)
+# i = 0
+# j = len(list_1)-1
+# n = 0
+# while i <= len(list_1)-1:
+#     while j >= i:
+#         if list_1[i] != list_1[j]:
+#             n += 1
+#         j -= 1 
+#     i += 1           
+# print(n)
+ 
+# на УРОКЕ
+# создание списка
+n = int(input('КВведите кол-во элементов: '))
+some_list = []
+for _ in range(n):
+    some_list.append(int(input()))
+print(some_list)
+# решение
+new_list = []
+for elem in some_list:
+    if elem not in new_list:
+        new_list.append(elem)
+print(len(new_list))
