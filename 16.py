fcac3b3b93ebdbf1baceb8053019c98e1bfafe70
Asy_student <asb_mogilev@mail.ru>
2023-05-29 19:52:41 +0300

    2 решение задачи 16

--- 16.py
+++ 16.py
@@ -17,9 +17,12 @@ for _ in range(n):
     some_list.append(int(input()))
 # print(some_list)
 x = int(input('Введите значение Х = '))
-count = 0
-# решение
-for element in some_list:
-    if element == x:
-        count += 1
-print(f"-> {count} ")
+# count = 0
+# решение 1
+# for element in some_list:
+#     if element == x:
+#         count += 1
+# print(f"-> {count} ")
+
+# решение 2
+print(some_list.count(x))
