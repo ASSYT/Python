2714e8cb3e0be87d78d999c01ac336fa7ffd3553
Asy_student <asb_mogilev@mail.ru>
2023-06-03 16:06:08 +0300

    Adding a task 26

--- 26.py
+++ 26.py
@@ -5,3 +5,13 @@
 
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+def degree_number (c, d):
+    if d == 0:
+        return 1
+    d -= 1
+    return c * degree_number (c, d)
+
+a = int (input("Введите первое число А = "))
+b = int (input("Введите второе число В = "))
+print(f"A = {a}; B = {b} -> {degree_number(a, b)} ({a} в степени {b})")
