bdf877ef8c0cdbc0a90f2651bf4196f4e050a9e8
Asy_student <asb_mogilev@mail.ru>
2023-06-07 08:04:26 +0300

    Addes task 32 с идеальным решение предложенным

--- 32.py
+++ 32.py
@@ -6,7 +6,7 @@
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9,
 #        0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
-
+'''
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 min = 7
 max = 10
@@ -16,3 +16,11 @@ for i in range(len(list_1)):
         list_2.append(i)
 
 print(list_2)
+'''
+# идеальное решение предложено
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i, end = ' ')
